52b238d6788e8fecbeaf3738b856203f68afc9bb
HeHo <128844537+User1123212@users.noreply.github.com>
2025-09-19 14:27:42 -0600

    Update TestEnemy.gd

--- WildLands/Scenes_Scripts/NPC/TestEnemy.gd
+++ WildLands/Scenes_Scripts/NPC/TestEnemy.gd
@@ -1,3 +1,4 @@
+#Here is the script for the enemy character
 extends CharacterBody2D
 
 
@@ -6,17 +7,20 @@ var target = null
 
 var hp = 100
 
-
+#Moves the enemy towards the target
 func _physics_process(delta):
 	if target != null:
 		position = position.move_toward(target.get_parent().position,delta*speed)
 	
 
 	move_and_slide()
+
+#Checks if enemy is dead and removes from main scene
 func _process(delta):
 	if hp <= 0:
 		self.queue_free()
 
+#Sets player as target when it enters the enemy's "Attention Zone" and removes the player as the target when outside of zone
 func _on_attention_zone_area_entered(area):
 	if area.is_in_group("Player"):
 		target = area
@@ -24,7 +28,7 @@ func _on_attention_zone_area_exited(area):
 	if area.is_in_group("Player"):
 		target = null
 
-
+#Adds knockback from attacks (Work in progress)
 func _on_hitbox_area_entered(area):
 	if area.is_in_group("Attack"):
 		hp -= 20
